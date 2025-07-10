8ee1ab18101586941af6252c2fbe335f5da94f5a
Ricardo Gabriel <rigabriel007@gmail.com>
2025-07-10 20:45:23 -0300

    Métodos

---
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -0,0 +1,32 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        if(this.saudeAtual - quantidadeDeDano < 0)
+            this.saudeAtual = 0;
+        else
+            this.saudeAtual -= quantidadeDeDano;    
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        if(this.saudeAtual + quantidadeDeCura > 100)
+            this.saudeAtual = 100;
+        else
+            this.saudeAtual += quantidadeDeCura;
+    }
+
+}
