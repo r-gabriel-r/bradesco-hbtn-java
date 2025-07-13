215f2ca6d7445f50cc6e65e2abb263552edb1c8b
Ricardo Gabriel <rigabriel007@gmail.com>
2025-07-13 09:15:29 -0300

    task 6

---
+++ Heranca/abstrata/Quadrado.java
@@ -0,0 +1,23 @@
+public class Quadrado extends Retangulo {
+
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if(lado < 0) {
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0"); 
+        }
+        this.lado = lado;
+    }
+
+    public String toString() {
+        return String.format("[Quadrado] %.2f", lado);
+    }
+
+    public double area() {
+        return lado * lado;
+    }
+}
---
+++ Heranca/abstrata/Retangulo.java
@@ -0,0 +1,29 @@
+public class Retangulo extends FormaGeometrica{
+    
+    protected double altura;
+    protected double largura;
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        if(altura < 0) {
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+        this.altura = altura;
+    }
+    public double getLargura() {
+        return largura;
+    }
+    public void setLargura(double largura) {
+        if(largura < 0) {
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = largura;
+    }
+
+    public double area() {
+        return altura * largura;
+    }
+
+}
