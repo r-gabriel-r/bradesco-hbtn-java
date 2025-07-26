fe05801e4d6be7a88bb5ec18c02e7b45ec6d6e81
Ricardo Gabriel <rigabriel007@gmail.com>
2025-07-26 16:07:04 -0300

    livraria

---
+++ 0x03/livraria/Livro.java
@@ -0,0 +1,50 @@
+import exceptions.AutorInvalidoException;
+import exceptions.LivroInvalidoException;
+
+public class Livro {
+    
+    private String titulo;
+    private String autor;
+    private double preco;
+    
+    public Livro(String titulo, String autor, double preco) throws LivroInvalidoException, AutorInvalidoException {
+        this.setTitulo(titulo);;
+        this.setAutor(autor);
+        this.setPreco(preco);;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) throws LivroInvalidoException {
+        if(titulo.length() < 3 ) {
+            throw new LivroInvalidoException("Titulo de livro invalido");
+        }
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) throws AutorInvalidoException {
+        if(!autor.contains(" ")) {
+            throw new AutorInvalidoException("Nome de autor invalido");
+        }
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) throws LivroInvalidoException {
+        if(preco < 0) {
+            throw new LivroInvalidoException("Preco de livro invalido");
+        }
+        this.preco = preco;
+    }
+
+    
+}
---
+++ 0x03/livraria/exceptions/AutorInvalidoException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class AutorInvalidoException extends Exception{
+    
+    public AutorInvalidoException(String msg) {
+        super(msg);
+    }
+
+}
