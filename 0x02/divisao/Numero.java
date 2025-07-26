8ce2c7ae7476f07179d411448a6bd55d4c06e16f
Ricardo Gabriel <rigabriel007@gmail.com>
2025-07-26 15:02:31 -0300

    correcao bloco finally

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -1,11 +1,13 @@
 public class Numero {
     
     public static void dividir(int a, int b) {
+        int resultado = 0;
         try {
-            System.out.printf("%d / %d = %d", a, b, a/b);
+            resultado = a/b;
         } catch(Exception e) {
             System.out.println("Nao eh possivel dividor por zero");
-            System.out.printf("%d / %d = %d", a, b, 0);
+        } finally {
+            System.out.printf("%d / %d = %d", a, b, resultado);
         }
     }
 }
