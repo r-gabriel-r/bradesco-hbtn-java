8d00144464c986a7b7b4f3dea972dc965621270b
Ricardo Gabriel <rigabriel007@gmail.com>
2025-08-03 10:20:30 -0300

    correção FileWritingExercise

--- arquivos/0/FileWritingExercise.java
+++ arquivos/0/FileWritingExercise.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 
  public class FileWritingExercise {
-     public static void main(String[] args) throws IOException {
+     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Solicita o nome do arquivo
@@ -15,18 +15,24 @@ import java.util.Scanner;
         // implemente o codigo aqui
         // criar arquivo
         File file = new File(fileName);
-        file.createNewFile();
-        FileWriter writer = new FileWriter(file);
-        // pedir texto do usuário
-        String texto = "";
-        while(!texto.equals("sair")) {
+        try {
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file);
+            // pedir texto do usuário
             System.out.print("Digite o texto: ");
-            texto = scanner.nextLine();
-            if(!texto.equals("sair"))
+            String texto = scanner.nextLine();
+            while(!texto.equals("sair")) {
+                if(texto.equals("sair"))
+                    break;
                 writer.append(texto + "\n");
+                System.out.print("Digite o texto: ");
+                texto = scanner.nextLine();
+            }
+            scanner.close();
+            writer.close();
+            System.out.println("Arquivo criado com sucesso");
+        } catch (IOException e) {
+            System.err.printf("Erro ao criar o arquivo: %s %s", fileName, e.getMessage());
         }
-        scanner.close();
-        writer.close();
-        System.out.println("Arquivo criado com sucesso");
      }
  }
