84aaaef9ba6e4db92b692cc2ffce48d91079d55b
Ricardo Gabriel <rigabriel007@gmail.com>
2025-08-10 14:34:29 -0300

    Complexidade N

---
+++ complexidade/1/PessoasArray.java
@@ -0,0 +1,34 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de buscaLinear
+    public void buscaLinear(String nome){
+        boolean encontrado = false;
+        System.out.printf("Procurando pelo nome: \"%s\"\n", nome);
+        for (int i=0;i<this.nomes.length; i++) {
+            System.out.printf("Passando pelo indice:%d\n",i);
+            if(this.nomes[i].equals(nome)) {
+                System.out.printf("Nome pesquisado é %s que está na posição %s\n", nome, i);
+                encontrado = true;
+            }
+        }
+        if(!encontrado)
+            throw new IllegalArgumentException("O nome " + nome +  " não se encontra no array de nomes");
+    }
+}
