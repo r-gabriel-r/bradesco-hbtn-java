b6c9b095094088a06a19aea6aebeb59fbeb1b15d
Ricardo Gabriel <rigabriel007@gmail.com>
2025-08-10 14:16:54 -0300

    Complexidade 1

---
+++ complexidade/0/PessoasArray.java
@@ -0,0 +1,27 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de pesquisaConstante
+    public void pesquisaTempoConstante(int id){
+        if(id > this.nomes.length) {
+            throw new IllegalArgumentException("O array de nomes possui :" + this.nomes.length + " nomes.");
+        }
+        System.out.printf("Nome pesquisado é %s que está na posição %d\n", this.nomes[id], id);
+    }
+}
