0ea36239da084a9f3e85d56d92f5cf9770ace731
Ricardo Gabriel <rigabriel007@gmail.com>
2025-08-10 10:17:09 -0300

    Teste alunos List

---
+++ estruturas_de_dados/1/GestaoAluno.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestaoAluno {
+
+    private List<Aluno> alunos = new ArrayList<Aluno>();
+
+    public void adicionarAluno(Aluno aluno) {
+        this.alunos.add(aluno);
+    }
+
+    public void excluirAluno(String nome) {
+
+        for (Aluno a : alunos) {
+            if (a.getNome().equals(nome)) {
+                this.alunos.remove(a);
+                System.out.printf("Aluno %s excluído com sucesso\n", nome);
+                return;
+            }
+        }
+        System.out.printf("Aluno %s não encontrado\n", nome);
+    }
+
+    public void buscarAluno(String nome) {
+        for (Aluno a : alunos) {
+            if (a.getNome().equals(nome)) {
+                System.out.println(a.toString());
+                return; 
+            }
+        }
+        System.out.printf("Aluno %s não encontrado\n", nome);
+    }
+
+    public void listarAlunos() {
+        for (Aluno aluno : alunos) {
+            System.out.println(aluno.toString());
+        }
+    }
+
+}
