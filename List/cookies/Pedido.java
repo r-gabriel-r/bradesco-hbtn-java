1efb0814a9b9a869c4b01ad577b306e7f4062a9c
Ricardo Gabriel <rigabriel007@gmail.com>
2025-08-17 14:45:18 -0300

    Exercicio lista cookies

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Pedido {
+
+    private List<PedidoCookie> cookies;
+
+    public Pedido() {
+        this.cookies = new ArrayList<>();
+    }
+    
+    public void adicionarPedidoCookie(PedidoCookie pedidoCookie) {
+        this.cookies.add(pedidoCookie);
+    }
+
+    public int obterTotalCaixas() {
+        int total = 0;
+        for (PedidoCookie pedidoCookie : cookies) {
+            total += pedidoCookie.getQuantidadeCaixas();
+        }
+        return total;
+    }
+
+    public int removerSabor(String sabor) {
+        int itensRemovidos = 0;
+        Iterator<PedidoCookie> iteratorCookies = cookies.iterator();
+        while(iteratorCookies.hasNext()) {
+            PedidoCookie pedido = iteratorCookies.next();
+            if(pedido.getSabor().equals(sabor)) {
+                itensRemovidos+= pedido.getQuantidadeCaixas();
+                iteratorCookies.remove();
+            }
+        }
+        return itensRemovidos;
+    }
+}
