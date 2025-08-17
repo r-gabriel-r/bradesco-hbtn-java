01f3da0b8ffb086005e71c0e6fbe439994f9cfb5
Ricardo Gabriel <rigabriel007@gmail.com>
2025-08-16 22:49:05 -0300

    exercicio generics

---
+++ Generics_Exercises/armazem/Comida.java
@@ -0,0 +1,17 @@
+public class Comida {
+
+    private String nome;
+    private double calorias;
+    private double preco;
+
+    public Comida(String nome, double calorias, double preco) {
+        this.nome = nome;
+        this.calorias = calorias;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %f R$ %f", this.nome, this.calorias, this.preco);
+    }
+}
