ac7fe28b59f76cabe4d79493389cbfe670c0d01d
Ricardo Gabriel <rigabriel007@gmail.com>
2025-09-14 11:32:47 -0300

    exercicio Comparator reversed

---
+++ Streams_Collectors_Optional/maior_preco/Produto.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+
+public class Produto {
+
+    private int codigo;
+
+    private String nome;
+
+    private CategoriaProduto categoria;
+
+    private double preco;
+
+    public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.ITALIAN , "[%d] %s %s R$ %.2f", this.codigo, this.nome, this.categoria, this.preco);
+    }
+
+}
