5b987cd48016a65d32e2d888acf412ad730cdb57
Ricardo Gabriel <rigabriel007@gmail.com>
2025-09-21 17:10:21 -0300

    exercicio Collectors com GroupBy e Comparator

--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -9,12 +9,10 @@ import java.util.stream.Collectors;
 public class ConsultaPessoas {
 
     public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> lista) {
-        System.err.println("-->" + lista + "<--");
         return lista.stream()
                 .collect(Collectors.groupingBy(Pessoa::getCargo,
                         () -> new TreeMap<>(Comparator.reverseOrder()),
                         Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Pessoa::getNome)))));
-
     }
 
 }
