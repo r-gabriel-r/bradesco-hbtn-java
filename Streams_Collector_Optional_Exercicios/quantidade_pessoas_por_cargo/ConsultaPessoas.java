8a534986af4aec03304a6d0e5dd65d617db63bc3
Ricardo Gabriel <rigabriel007@gmail.com>
2025-09-28 08:44:56 -0300

    Exercicio Collectors GroupBy e Counting

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,23 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> lista) {
+        return lista.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo,
+                        () -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Pessoa::getNome)))));
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> lista) {
+        return lista.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, 
+                        Collectors.counting()));
+    }
+
+}
