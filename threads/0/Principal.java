00f9a9cccbe1e037627db277380dc88c93e8e77e
Ricardo Gabriel <rigabriel007@gmail.com>
2025-09-28 13:20:55 -0300

    Thread com variável synchronized

---
+++ threads/0/Principal.java
@@ -0,0 +1,19 @@
+public class Principal {
+
+    public static void main(String[] args) throws InterruptedException{
+
+        Contador contador = new Contador();
+        
+        ThreadContador thread1 = new ThreadContador(contador);
+        ThreadContador thread2 = new ThreadContador(contador);
+
+        thread1.start();
+        thread1.join();
+        thread2.start();
+        thread2.join();
+
+        System.out.printf("Valor final do contador: %d", contador.getContagem());
+
+    }
+    
+}
---
+++ threads/0/ThreadContador.java
@@ -0,0 +1,19 @@
+class ThreadContador extends Thread {
+    private Contador contador;
+
+    public ThreadContador(Contador contador) {
+        this.contador = contador;
+    }
+
+    public Contador getContador() {
+        return contador;
+    }
+
+    @Override
+    public void run() {
+        // Cada thread incrementa o contador 1000 vezes
+        for (int i = 0; i < 1000; i++) {
+            contador.incrementar();
+        }
+    }
+}
